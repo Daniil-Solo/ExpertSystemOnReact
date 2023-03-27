d4f64505401b48e066e186ca141ae4c1124578e7
Daniil-Solo <daniil.solo1723@gmail.com>
2023-03-28 00:37:45 +0500

    add document-actions for top menu

---
+++ src/components/FileAction.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function FileAction(props){
+    const {actions} = props;
+    const [isOpenedActionList, setIsOpenedActionList] = React.useState(false);
+    return(
+        <>
+            <div style={{backgroundColor: "#115293", width: "40px", height: "40px", borderTopLeftRadius: "16px", borderTopRightRadius: "16px", cursor: "pointer", position: "absolute", bottom: "0", right: "72px", display: "flex", alignItems: "center", justifyContent: "center"}} onMouseEnter={() => setIsOpenedActionList(true)} onMouseLeave={() => setIsOpenedActionList(false)}>
+                <img src="document.svg" alt="Документ" />
+            </div>
+            {isOpenedActionList &&
+                <div style={{position: "absolute", bottom: "-101px", right: "45px", borderBottomLeftRadius: "4px", borderBottomRightRadius: "4px", borderWidth: "1px", borderTopWidth: "0", borderStyle: "solid", backgroundColor: "#FFFFFF", height: "100px", width: "96px", borderColor: "#E6ECF4", display: "flex", flexDirection: "column", justifyContent: "space-around"}} onMouseEnter={() => setIsOpenedActionList(true)} onMouseLeave={() => setIsOpenedActionList(false)}>
+                    {
+                        actions.map(
+                            action => 
+                            <p style={{color: "#000", margin: "0", paddingLeft: "8px", fontSize: "16px", cursor: "pointer"}} onClick={action.handler}>
+                                {action.title}
+                            </p>
+                        )
+                    }
+                </div>
+            }
+        </>
+    )
+}
+
+export default FileAction;
--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,19 +1,34 @@
 import MenuTab from "./MenuTab";
+import FileAction from "./FileAction";
 
 function TopMenu(props) {
     const {currentTabIndex, changeCurrentTabIndex} = props;
     const tabTitles = ["Общая информация", "Правила", "Переменные", "Домены"]
+    const actions = [
+        {
+            title: "Создать...",
+            handler: () => alert("Создаю")
+        },
+        {
+            title: "Открыть...",
+            handler: () => alert("Открываю")
+        },
+        {
+            title: "Сохранить...",
+            handler: () => alert("Сохраняю")
+        },
+    ]
     return (
-      <div style={{backgroundColor: "#64B5F6", height: "64px", margin: "0", padding: "0", display: "flex", flexDirection: "column", justifyContent: "flex-end"}}> 
-        <div style={{width: "878px", margin: "0 auto", display: "flex", justifyContent: "space-around", alignItems: "flex-end"}}>
-            {
-                tabTitles.map(
-                    (tabTitle, index) => <MenuTab title={tabTitle} value={index} isActive={currentTabIndex === index} changeCurrentTabIndex={changeCurrentTabIndex}/>
-                )
-            }
+        <div style={{backgroundColor: "#64B5F6", height: "64px", margin: "0", padding: "0", display: "flex", flexDirection: "column", justifyContent: "flex-end", position: "relative"}}> 
+            <div style={{width: "878px", margin: "0 auto", display: "flex", justifyContent: "space-around", alignItems: "flex-end"}}>
+                {
+                    tabTitles.map(
+                        (tabTitle, index) => <MenuTab title={tabTitle} value={index} isActive={currentTabIndex === index} changeCurrentTabIndex={changeCurrentTabIndex}/>
+                    )
+                }
+            </div>
+            <FileAction actions={actions}/>
         </div>
-        
-      </div>
     );
   }
   
