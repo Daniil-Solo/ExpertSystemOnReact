d138fa354c793efd1142061201d743549c82f12d
Daniil-Solo <daniil.solo1723@gmail.com>
2023-04-01 14:39:35 +0500

    add options and z-index for Select

--- src/components/UI/Select.jsx
+++ src/components/UI/Select.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 
 function Select(props){
-    const {activeValue, values, handleValue} = props;
+    const {activeValue, setActiveValue, options} = props;
     const [isOpenedList, setIsOpenedList] = React.useState(false);
+    const activeOption = options.find(option => option.value === activeValue);
+    const activeLabel = activeOption? activeOption.label: "Выбрать";
 
     return (
         <div style={{backgroundColor: "#FFFFFF", color: "#000000", position: "relative", width: "100%", height: "24px", borderRadius: "4px", borderWidth: "1px", borderStyle: "solid", borderColor: "#E6ECF4", fontSize: "14px", cursor: "pointer", maxWidth: "200px"}} onClick={() => setIsOpenedList(!isOpenedList)}>
-            <p style={{margin: "4px 16px", overflow: "hidden", whiteSpace: "nowrap", padding: "0 16px 0 0"}}>{activeValue}</p>
+            <p style={{margin: "4px 16px", overflow: "hidden", whiteSpace: "nowrap", padding: "0 16px 0 0"}}>
+                {activeLabel}
+            </p>
             {
                 isOpenedList &&
-                <div style={{position: "absolute", top: "24px", right: "-1px", width: "100%", backgroundColor: "#FFFFFF", color: "#000000", borderRadius: "4px", borderWidth: "1px", borderStyle: "solid", borderColor: "#E6ECF4", display: "flex", flexDirection: "column", gap: "4px"}} onMouseLeave={() => setIsOpenedList(false)}>
+                <div style={{position: "absolute", top: "24px", right: "-1px", width: "100%", backgroundColor: "#FFFFFF", color: "#000000", borderRadius: "4px", borderWidth: "1px", borderStyle: "solid", borderColor: "#E6ECF4", display: "flex", flexDirection: "column", gap: "4px", zIndex: 3}} onMouseLeave={() => setIsOpenedList(false)}>
                     {
-                        values.map(
-                            value => 
-                            <p key={value} style={{margin: "4px 16px", padding: "0"}} onClick={() => handleValue(value)}>
-                                {value}
+                        options.map(
+                            option => 
+                            <p key={option.value} style={{margin: "4px 16px", padding: "0"}} onClick={() => setActiveValue(option.value)}>
+                                {option.label}
                             </p>
                         )
                     }
