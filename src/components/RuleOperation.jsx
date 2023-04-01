871ea299eef0193c80de99aebe79be9889feaec2
Daniil-Solo <daniil.solo1723@gmail.com>
2023-04-01 14:44:01 +0500

    add options-generation for Selects in RuleOperation

--- src/components/RuleOperation.jsx
+++ src/components/RuleOperation.jsx
@@ -1,17 +1,41 @@
-import Select from "./Select";
+import Select from "./UI/Select";
+import React from "react";
 
-function RuleOption(props){
-    const {activeValue1, values1, handleValue1, activeValue2, values2, handleValue2, handleDelete} = props;
+function RuleOperation(props){
+    const {variables, domains, activeVariable, setVariable, activeDomainValue, setDomainValue, handleDelete} = props;
+    const variableOptions = variables.map(variable => {
+        return {
+          label: variable.label,
+          value: variable.name
+        }
+    });
+    const getDomainValueOptions = () => {
+        const variable = variables.find(item => item.name === activeVariable);
+        if (!variable)
+            return []
+        const domain = domains.find(item => item.name === variable.domain);
+        if (!domain)
+            return []
+        return domain.domainValues.map(item => {
+            return {
+                label: item.label,
+                value: item.value
+            }
+        })
+    }
+    const domainValueOptions = getDomainValueOptions();
+    
+
     return (
-        <div style={{borderWidth: "1px", borderStyle: "solid", backgroundColor: "#FFFFFF", borderColor: "#E6ECF4", borderRadius: "4px", padding: "12px 16px", display: "flex", flexDirection: "row", gap: "8px"}}>
+        <div style={{borderWidth: "1px", borderStyle: "solid", backgroundColor: "#FFFFFF", borderColor: "#E6ECF4", borderRadius: "4px", padding: "12px 16px", display: "flex", flexDirection: "row", gap: "16px"}}>
             <div style={{flex: "1 1 auto", display: "flex", gap: "4px", alignItems: "center"}}>
-                <Select activeValue={activeValue1} values={values1} handleValue={handleValue1}/>
+                <Select activeValue={activeVariable} options={variableOptions} setActiveValue={setVariable}/>
                 <p style={{width: "16px", height: "16px", borderWidth: "1px", borderStyle: "solid", borderRadius: "4px", backgroundColor: "#1976D2", color: "#FFFFFF", display: "flex", alignItems: "center", justifyContent: "center", margin: "0", fontWeight: "bold", padding: "0 4px"}}>=</p>
-                <Select activeValue={activeValue2} values={values2} handleValue={handleValue2}/>
+                <Select activeValue={activeDomainValue} options={domainValueOptions} setActiveValue={setDomainValue}/>
             </div>
             <img style={{cursor: "pointer"}} src="delete.svg" alt="" onClick={handleDelete} />
         </div>
     )
 }
 
-export default RuleOption;
+export default RuleOperation;
