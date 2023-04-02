e0d86d93cc61f63836e2f69c871b7bfc59fc2314
Daniil-Solo <daniil.solo1723@gmail.com>
2023-04-02 16:19:39 +0500

    add title in Select

--- src/components/UI/Select.jsx
+++ src/components/UI/Select.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 function Select(props){
-    const {activeValue, setActiveValue, options} = props;
+    const {activeValue, setActiveValue, options, title} = props;
     const [isOpenedList, setIsOpenedList] = React.useState(false);
     const activeOption = options.find(option => option.value === activeValue);
     const activeLabel = activeOption? activeOption.label: "Выбрать";
 
     return (
-        <div style={{backgroundColor: "#FFFFFF", color: "#000000", position: "relative", width: "100%", height: "24px", borderRadius: "4px", borderWidth: "1px", borderStyle: "solid", borderColor: "#E6ECF4", fontSize: "14px", cursor: "pointer", maxWidth: "200px"}} onClick={() => setIsOpenedList(!isOpenedList)}>
-            <p style={{margin: "4px 16px", overflow: "hidden", whiteSpace: "nowrap", padding: "0 16px 0 0"}}>
+        <div style={{backgroundColor: "#FFFFFF", color: "#000000", position: "relative", width: "100%", height: "24px", borderRadius: "4px", borderWidth: "1px", borderStyle: "solid", borderColor: "#E6ECF4", fontSize: "14px", cursor: "pointer", maxWidth: "200px", position: "relative"}} onClick={() => setIsOpenedList(!isOpenedList)}>
+            {
+                title &&
+                <p style={{position: "absolute", left: "16px", top: "-9px", margin: "0", backgroundColor: "#FFFFFF", color: "#797979", fontSize: "14px"}}>
+                    {title}
+                </p>
+            }
+            
+            <p style={{margin: title? "8px 16px 4px": "4px 16px", overflow: "hidden", whiteSpace: "nowrap", padding: "0 16px 0 0"}}>
                 {activeLabel}
             </p>
             {
