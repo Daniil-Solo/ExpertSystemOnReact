a59a78b81af655c3e6772461333cdbb8779e5d18
Daniil-Solo <daniil.solo1723@gmail.com>
2023-04-05 17:48:08 +0500

    add Modal

---
+++ src/components/UI/Modal/Modal.jsx
@@ -0,0 +1,28 @@
+import Button from "../Button";
+import "./modal.css"
+function Modal(props){
+    const {isActive, setIsActive, addElement, title, children} = props;
+    const closeModal = () => {
+        setIsActive(false);
+    }
+    return (
+        <div className={isActive? "modal active": "modal"} onClick={closeModal}>
+            <div className={isActive? "modal__content active": "modal__content"} onClick={e => e.stopPropagation()}>
+                <p className="modal__header">
+                    {title}
+                    <img src="close.svg" alt="" className="modal__close" onClick={closeModal}/>
+                </p>
+                <div className="modal__body">
+                    {children}
+                </div>
+                <div className="modal__footer">
+                    <Button title="Добавить" handleClick={addElement} buttonType="success"/>
+                    <Button title="Отмена" handleClick={closeModal} buttonType="danger"/>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Modal;
